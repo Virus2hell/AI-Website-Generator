f285e48237ff5fa7b06ae65bc9bbdcd9c9823320
Virus2hell <atharvanarvekar269@gmail.com>
2025-12-10 13:45:19 +0530

    adding header section

---
+++ app/_components/header.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+
+const MenuOptions = [
+    {
+        name: 'Pricing',
+        path: '/pricing'
+    },
+    {
+        name: 'Contact us',
+        path: '/contact-us'
+    }
+]
+
+function header() {
+  return (
+    <div className='flex items-center justify-between p-4 shadow-md'>
+        {/* {logo} */}
+        <div className='flex gap-2 items-center'>
+            <Image src="/logo.svg" alt="Logo" width={35} height={35} />
+            <h2 className='font-bold text-xl'>AI Website Generator</h2>
+        </div>
+        {/* {menu options} */}
+        <div className='flex gap-4'>
+            {MenuOptions.map((menu, index) => (
+                <Button variant={"ghost"} key={index}>{menu.name}</Button>
+            ))}
+        </div>
+
+        {/* {get started button} */}
+        <Button>Get Started <ArrowRight/></Button>
+    </div>
+  )
+}
+
+export default header

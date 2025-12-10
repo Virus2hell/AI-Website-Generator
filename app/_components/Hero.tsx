84f4a0907724fcadf773853f3acb7bf30e684bae
Virus2hell <atharvanarvekar269@gmail.com>
2025-12-10 15:17:16 +0530

    updated landing page

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { ImagePlus, ArrowUp, LayoutDashboard, Key, HomeIcon, User } from 'lucide-react'
+
+const suggestions = [
+    {
+        label:'Dashboard',
+        prompt:'Create a analytics dashboard to track customers and revenue data for a SaaS',
+        icon: LayoutDashboard
+    },
+    {
+        label:'Signup Form',
+        prompt:'Create a modern signup form with email and password fields, Google and Github login options and terms checkbox',
+        icon: Key
+    },
+    {
+        label: 'Hero',
+        prompt: 'Create a modern header and centered hero section for a productivity SaaS, Include a badge announcement a title with a subtle gradient effect, subtle, CTA, small social proof and an image',
+        icon: HomeIcon
+    },
+    {
+        label: 'User Profile Card',
+        prompt: 'Create a user profile card component for a social media website',
+        icon: User
+    }
+]
 
 function Hero() {
+
+    const [userInput, setUserInput] = useState<string>()
+    
   return (
     <div className='flex flex-col items-center h-[80vh] justify-center'>
         {/* Header and description  */}
@@ -8,13 +37,27 @@ function Hero() {
         <p className='mt-2 text-xl text-gray-500'>Generate, Edit and Explore design with AI, Export code as well</p>
 
         {/* input box */}
-        <div className='w-full max-w-xl p-5 border mt-5 rounded-2xl'>
+        <div className='w-full max-w-2xl p-5 border-4 mt-5 rounded-2xl'>
             <textarea placeholder='Describe your page design'
+            value={userInput}
+            onChange={(event) => setUserInput(event.target.value)}
             className='w-full h-24 focus:outline-none focus:ring-0 resize-none'
             />
+            <div className='flex justify-between items-center'>
+                <Button variant={"ghost"} size={'lg'}><ImagePlus /></Button>
+                <Button ><ArrowUp /></Button>
+            </div>
         </div>
 
         {/* suggestion */}
+        <div className='mt-4 flex gap-2'>
+            {suggestions.map((suggestion, index) => (
+                <Button key={index} variant={'outline'}
+                onClick={() => setUserInput(suggestion.prompt)}>
+                    <suggestion.icon/>
+                    {suggestion.label}</Button>
+            ))}
+        </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './_components/header'
+import Header from './_components/Header'
 import Hero from './_components/Hero'
 
 export default function home(){

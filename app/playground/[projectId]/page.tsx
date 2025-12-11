33c7c7b96888160b8f106e7e730cf1c3f1a90757
Virus2hell <atharvanarvekar269@gmail.com>
2025-12-11 19:35:04 +0530

    commit

--- app/playground/[projectId]/page.tsx
+++ app/playground/[projectId]/page.tsx
@@ -1,10 +1,15 @@
+"use client"
 import React from 'react'
 import PlaygroundHeader from '../_components/PlaygroundHeader'
 import ChatSection from '../_components/ChatSection'
 import WebsiteDesign from '../_components/WebsiteDesign'
 import ElementSettingSection from '../_components/ElementSettingSection'
+import { useParams, useSearchParams } from 'next/navigation'
 
 function Playground() {
+    const {projectId} = useParams();
+    const params = useSearchParams();
+    const frameId = params.get('frameId');
   return (
     <div>
         <PlaygroundHeader/>
@@ -16,7 +21,7 @@ function Playground() {
             <WebsiteDesign/>
 
             {/* setting section  */}
-            <ElementSettingSection/>
+            {/* <ElementSettingSection/> */}
         </div>
     </div>
   )
